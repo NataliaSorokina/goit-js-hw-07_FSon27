2509f8a5bd8e749a575b36c50ad984b0f62f2221
Natalia Sorokina <pticasonabird@gmail.com>
2021-01-27 22:52:31 +0300

    задание 2

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+/* Написать скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию 
+в список ul.ingredients. Для создания DOM-узлов использовать document.createElement(). */
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const liRef = ingredients.map(ingredient => {
+    const itemRef = document.createElement('li');
+    itemRef.textContent = ingredient;
+    return itemRef;
+});
+
+const ingredientsList = document.querySelector('ul#ingredients');
+ingredientsList.append(...liRef);
+console.log(ingredientsList);
